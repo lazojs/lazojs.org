d26dd7b7c802ce3285b2fbf93923bcaa8118e84c
Mike Davis <mike@michaeljacobdavis.com>
2015-01-09 17:04:56 -0800

    Use lazo docs from node_modules

--- docs/UpdatingDocs.md
+++ docs/UpdatingDocs.md
@@ -1,7 +1,5 @@
 Updating the API Documentation
 ------------------------------
 
-1. In [LazoJs](https://github.com/walmartlabs/lazojs), run the `grunt docs` to generate the `hbs` files.
-1. Navigate to `/docs/generated/docs/` in `lazojs` and grab the `hbs` files.
-1. Copy and paste them to `src/components/contets/views/api/partials` in this repo.
-1. Run `gulp`.
+1. Update to the lazo version with the docs you want.
+1. Run `gulp docs`.
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
+var rename = require('gulp-rename');
 var path = require('path');
 var sass = require('gulp-sass');
+var markdown = require('gulp-markdown');
 var lazo = path.normalize('.' + path.sep + 'node_modules' + path.sep + 'lazo' + path.sep + 'lazo.js');
 
 gulp.task('lint', function () {
@@ -29,6 +31,15 @@ gulp.task('sass', function () {
     return gulp.src('./src/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('generate-docs', function () {
+    return gulp.src('./node_modules/lazo/docs/*.md')
+        .pipe(markdown())
+        .pipe(rename(function(path){
+            path.extname = '.hbs';
+        }))
+        .pipe(gulp.dest('./src/components/content/views/api/partials/'));
 });
 
 gulp.task('default', ['sass', 'copy'], function(){
