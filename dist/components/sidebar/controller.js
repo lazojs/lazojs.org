d7be46a7293666a029a85ba03c9467e96182d18c
Jason Strimpel <jstrimpel@gmail.com>
2015-04-09 17:50:11 -0700

    upgraded to lazo v2

--- dist/components/sidebar/controller.js
+++ dist/components/sidebar/controller.js
@@ -2,6 +2,22 @@ define(['app/controller'], function (Ctl) {
 
     'use strict';
 
-    return Ctl.extend({});
+    return Ctl.extend({
+
+        index: function (options) {
+            var self = this;
+            this.loadCollection('content', {
+                params: {
+                    path: this.ctx.params.path + 'sidebar.json'
+                },
+                success: function (collection) {
+                    self.ctx.collections.sidebar = collection;
+                    Ctl.prototype.index.call(self, options);
+                },
+                error: options.error
+            });
+        }
+
+    });
 
 });
